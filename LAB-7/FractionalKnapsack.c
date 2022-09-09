d0075dd70da098d328c57ac1b9e869f22a24aaa1
Archit Kandu <architkandu@hotmail.com>
2022-09-09 20:28:48 +0530

    Update FractionalKnapsack.c

--- LAB-7/FractionalKnapsack.c
+++ LAB-7/FractionalKnapsack.c
@@ -2,25 +2,25 @@
 void knapsack(int n, float weight[], float profit[], float capacity) {
     float x[20], tp = 0;
     int i, j, u;
-    u = capacity;
-    for (i = 0; i < n; i++)
-        x[i] = 0.0;
-    for (i = 0; i < n; i++) {
-        if (weight[i] > u)
+    u=capacity;
+    for(i=0;i<n;i++)
+        x[i]=0.0;
+    for (i=0;i<n;i++) {
+        if(weight[i]>u)
             break;
-        else {
-            x[i] = 1.0;
-            tp = tp + profit[i];
-            u = u - weight[i];
+        else{
+            x[i]=1.0;
+            tp=tp+profit[i];
+            u=u-weight[i];
         }
     }
-    if (i < n)
-        x[i] = u / weight[i];
-    tp = tp + (x[i] * profit[i]);
-    printf("\nThe result vector is:- ");
-    for (i = 0; i < n; i++)
-        printf("%.2f ", x[i]);
-    printf("\nMaximum profit is:- %.2f", tp);
+    if(i<n)
+        x[i]=u/weight[i];
+    tp=tp+(x[i]*profit[i]);
+    printf("\nThe result vector is: ");
+    for(i=0;i<n;i++)
+        printf("%.2f ",x[i]);
+    printf("\nMaximum profit is: %.2f",tp);
 
 }
 int main() {
@@ -30,28 +30,26 @@ int main() {
     printf("\nEnter the no. of objects: ");
     scanf("%d", &num);
     printf("Enter the wts and profits of each object: ");
-    for (i = 0; i < num; i++) {
+    for(i = 0; i < num; i++) 
         scanf("%f %f", &weight[i], &profit[i]);
-    }
     printf("Enter the capacity of knapsack: ");
     scanf("%f", &capacity);
-    for (i = 0; i < num; i++) {
-        ratio[i] = profit[i] / weight[i];
-    }
-    for (i = 0; i < num; i++) {
-        for (j = i + 1; j < num; j++) {
-            if (ratio[i] < ratio[j]) {
-                temp = ratio[j];
-                ratio[j] = ratio[i];
-                ratio[i] = temp;
+    for(i=0;i<num;i++)
+        ratio[i]=profit[i]/weight[i];
+    for (i=0;i<num;i++) {
+        for(j=i+1;j<num;j++) {
+            if (ratio[i]<ratio[j]) {
+                temp=ratio[j];
+                ratio[j]=ratio[i];
+                ratio[i]=temp;
 
-                temp = weight[j];
-                weight[j] = weight[i];
-                weight[i] = temp;
+                temp=weight[j];
+                weight[j]=weight[i];
+                weight[i]=temp;
 
-                temp = profit[j];
-                profit[j] = profit[i];
-                profit[i] = temp;
+                temp=profit[j];
+                profit[j]=profit[i];
+                profit[i]=temp;
             }
         }
     }
