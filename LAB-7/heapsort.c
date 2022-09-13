c8d9b406039c91e666993bc0c2785503a3d99e7f
Archit Kandu <architkandu@hotmail.com>
2022-09-13 19:01:40 +0530

    Update heapsort.c

--- LAB-7/heapsort.c
+++ LAB-7/heapsort.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-void swap(int *a, int *b) {
+void swap(int *a,int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
@@ -15,21 +15,21 @@ void heapify(int arr[],int n,int i) {
     if (right<n && arr[right]>arr[largest])
         largest=right;
     if (largest!=i) {
-        swap(&arr[i], &arr[largest]);
-        heapify(arr, n, largest);
+        swap(&arr[i],&arr[largest]);
+        heapify(arr,n,largest);
     }
 }  
-void heapSort(int arr[], int n){   
+void heapSort(int arr[],int n){   
     for(int i=n/2-1;i>=0;i--)
         heapify(arr,n,i);
     for(int i=n-1;i>=0;i--) {
-        swap(&arr[0], &arr[i]);
-        heapify(arr, i, 0);
+        swap(&arr[0],&arr[i]);
+        heapify(arr,i,0);
     }
-  }
+}
 void printArray(int arr[], int n) {
-    for (int i = 0;i<n;++i)
-        printf("%d ", arr[i]);
+    for (int i=0;i<n;++i)
+        printf("%d ",arr[i]);
 }
 int main() {
     int arr[100],n;
